ee316f908834cdb39279b03cf47b2ecd636c4fd3
omywa <845777520@qq.com>
2014-08-12 18:54:49 +0800

    Create try.py

---
+++ try.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# generate random 10-dim vector
+row = int(raw_input("Total number of points:"))
+col = int(raw_input("Demension of each vectors:"))
+a=[[0 for r in range(row)]for c in range(col)]
+for i in range(5):
+    a[i]= np.random.random_integers(100,size=8)
+    print a[i]
+
+#calculate L2 distances
